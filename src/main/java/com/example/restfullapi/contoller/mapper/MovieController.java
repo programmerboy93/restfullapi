195271315134cd8e9696fcfe0ec520d49c3c0d6d
Łukasz Sikorski <lukaszsikorski19933@gmail.com>
2022-02-25 17:44:23 +0100

    Add three controllers actor, director, movie

---
+++ src/main/java/com/example/restfullapi/contoller/mapper/MovieController.java
@@ -0,0 +1,12 @@
+package com.example.restfullapi.contoller.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/movies")
+public class MovieController {
+
+}
